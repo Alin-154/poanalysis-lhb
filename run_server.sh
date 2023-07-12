c3c45f71a38795d94b0b2fca7d612a10ff65b020
nicholas wu <nicholas_wu@aliyun.com>
2023-07-12 14:41:41 +0800

    编写sh

--- run_server.sh
+++ run_server.sh
@@ -1,12 +1,8 @@
-
-model_name_or_path
-HOST="0.0.0.0"
-POST=6666
-MODEL_NAME_OR_PATH=""
-PTUNING_CHECKPOINT=""
+MODEL_NAME_OR_PATH=./
+PTUNING_CHECKPOINT=./
 PRE_SEQ_LEN=128
 
-CUDA_VISIBLE_DEVICES=0 python3 web.py \
-    --model_name_or_path "./model_dir/thudm/chatglm-6b/" \
-    --ptuning_checkpoint "./output2/output/sim-chatglm-6b-pt-128-2e-2/checkpoint-1800" \
+CUDA_VISIBLE_DEVICES=0 python server.py \
+    --model_name_or_path $MODEL_NAME_OR_PATH \
+    --ptuning_checkpoint $PTUNING_CHECKPOINT \
     --pre_seq_len $PRE_SEQ_LEN
